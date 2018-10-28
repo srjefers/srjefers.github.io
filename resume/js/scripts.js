72108e298000bb7e8101313148aaeabc5345a6b6
srjefers <ajefersonsti@gmail.com>
2018-10-28 13:33:55 -0600

    Inicia fase beta. aun faltan animaciones

--- resume/js/scripts.js
+++ resume/js/scripts.js
@@ -1,6 +1,5 @@
 const URL = 'https://raw.githubusercontent.com/srjefers/resume/master/js/';
 const menu = document.getElementById('menu');
-
 const miInit = {
     method: 'GET',
     cache: 'default'
@@ -43,11 +42,63 @@ function contruyeElementos(data){
     
 }
 
-menu.addEventListener('click',(e)=>{
-    let nav = document.getElementsByTagName('nav');
-    console.log("---");
-    nav[0].className += 'show';
+const nav = document.getElementById('n-dos');
+const barra = document.getElementsByClassName('barra');
+const mClose = document.getElementsByClassName('m_close');
+let cont = 0;
+menu.addEventListener('click',()=>{
+    if(cont == 0){
+        nav.style.minHeight = '100vh';
+        nav.style.display = 'flex';
+        menu.childNodes[1].textContent = 'Cerrar';
+        for(const elem of barra){
+            elem.classList.add('b-on');
+        }    
+        cont++;
+    }else{  
+        close();
+        cont--;
+    }
 });
 
+for(const btn_nav of mClose){
+    btn_nav.addEventListener('click',()=>{
+        close();
+        cont--;
+    });
+}
+function close(){
+    menu.childNodes[1].textContent = 'Menu';
+    nav.style.minHeight = '0vh';
+    nav.style.display = 'none';
+    for(const elem of barra){
+        elem.classList.remove('b-on');
+    }  
+}
 
+const box = document.getElementsByClassName('box');
+function animate(){
+    for(let i = 0; i < box.length; i++){
+        let a = box[i].children[1];
+        let b;
+        box[i].addEventListener('mouseover',() => {
+            b = a.children[0];
+            let c = b.children[0];
+            if (c.children[1] != null){
+                c.childNodes[3].style.opacity = 1;
+                c.childNodes[3].style.marginLeft = '0.5em';
+            }
+        });
+        box[i].addEventListener('mouseout',() => {
+            b = a.children[0];
+            let c = b.children[0];
+            if (c.children[1] != null){
+                c.childNodes[3].style.opacity = 0;
+                c.childNodes[3].style.marginLeft = '-0.5em';
+            }
+        });
+
+    }
+}
+animate();
 
